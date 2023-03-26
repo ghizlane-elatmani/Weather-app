38723fdb7f9fde1c9048e6dc4c9d6a6f6be5ac21
Ghizlane <ghizlane.elatmani.99@gmail.com>
2023-03-26 16:30:00 +0000

    2.0.0: Install tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import "./App.scss";
-import Sidebar from "./components/Sidebar/Sidebar";
-import Forecast from "./components/Forecast/Forecast";
-
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <Sidebar />
-        <Forecast />
-      </div>
+    <div className="">
+      <div className=""></div>
     </div>
   );
 }
